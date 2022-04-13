8c661606cf965f67c05930f8205631bfd9d944a4
yngtdd <yngtdd@gmail.com>
2022-04-13 17:45:06 -0400

    Refactor arg parsing to new function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
 use clap::{App, Arg};
 use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
-fn main() {
+fn parse_args() -> clap::ArgMatches<'static> {
     let args = App::new("grep-lite")
         .version("0.1")
         .about("Searches for patterns")
@@ -22,12 +22,16 @@ fn main() {
         )
         .get_matches();
 
-    let filename = args.value_of("file").unwrap();
+    args
+}
+
+fn main() {
+    let args = parse_args();
+    let filename = args.value_of("file").expect("Need file to parse");
     let f = File::open(filename).expect("Oops, where is the file?");
     let reader = BufReader::new(f);
-
-    let pattern = args.value_of("pattern").unwrap();
-    let re = Regex::new(pattern).unwrap();
+    let pattern = args.value_of("pattern").expect("No pattern given");
+    let re = Regex::new(pattern).expect("Cannot build regex");
 
     for (line_num, line_) in reader.lines().enumerate() {
         let line = line_.unwrap();
