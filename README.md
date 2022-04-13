440f50d44d11e0c2184bcfcd3b8b024039605480
yngtdd <yngtdd@gmail.com>
2022-04-13 11:36:27 -0400

    Add read from file

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Grep Lite
 
 A toy version of Grep.
+
+## Example
+
+"Every face, every shop, bedroom window, public-house and 
+dark square is a picture feverishly turned--in search of what?
+It is the same with books.
+What do we seek through millions of pages?"
+
+- Virginia Wolf
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,37 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use clap::{App, Arg};
+use regex::Regex;
+
 fn main() {
-    let quote = "\
-        Every face, every shop, bedroom window, public-house and 
-        dark square is a picture feverishly turned--in serach of what?
-        It is the same with books.
-        What do we seek through millions of pages?";
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("Searches for patterns")
+        .arg(
+            Arg::with_name("file")
+                .help("The file to search within")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
 
-    let ctx_lines = 1;
-    let needle = "dark";
-    let mut tags: Vec<usize> = vec![];
-    let mut ctx: Vec<Vec<(usize, String)>> = vec![];
+    let filename = args.value_of("file").unwrap();
+    let f = File::open(filename).expect("Oops, where is the file?");
+    let reader = BufReader::new(f);
 
-    for (i, line) in quote.lines().enumerate() {
-        if line.contains(needle) {
-            tags.push(i);
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
 
-            let v = Vec::with_capacity(2 * ctx_lines + 1);
-            ctx.push(v);
-        }
-    }
-
-    if tags.is_empty() {
-        return;
-    }
-
-    for (i, line) in quote.lines().enumerate() {
-        for (j, tag) in tags.iter().enumerate() {
-            let lower_bound = tag.saturating_sub(ctx_lines);
-            let upper_bound = tag + ctx_lines;
-
-            if (i >= lower_bound) && (i <= upper_bound) {
-                let line_as_string = String::from(line);
-                let local_ctx = (i, line_as_string);
-                ctx[j].push(local_ctx);
-            }
-        }
-    }
-
-    for local_ctx in ctx.iter() {
-        for &(i, ref line) in local_ctx.iter() {
-            let line_num = i + 1;
+    for (line_num, line_) in reader.lines().enumerate() {
+        let line = line_.unwrap();
+        if re.find(&line).is_some() {
             println!("{}: {}", line_num, line);
         }
     }
